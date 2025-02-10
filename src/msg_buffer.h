a7028a952b14384f7e83337750925481153df91b
LT-Schmiddy <schmidalex1996@gmail.com>
2025-02-09 23:23:11 -0500

    adding declarations for get/set header

--- src/msg_buffer.h
+++ src/msg_buffer.h
@@ -23,7 +23,23 @@ typedef union {
 } MsgBuffer;
 
 s32 MsgBuffer_Len(MsgBuffer* buf);
+s32 MsgBuffer_ContentLen(MsgBuffer* buf);
 s32 MsgBuffer_CreateFromStr(MsgBuffer* dst, char* src);
 s32 MsgBuffer_CreateFromStr_PipeEscapeBytes(MsgBuffer* dst, char* src);
+
+// Getters/Setters for the header:
+u8 MsgBuffer_GetTextBoxType(MsgBuffer* buf);
+void MsgBuffer_SetTextBoxType(MsgBuffer* buf, u8 type);
+u8 MsgBuffer_GetTextBoxYPos(MsgBuffer* buf);
+void MsgBuffer_SetTextBoxYPos(MsgBuffer* buf, u8 pos);
+u8 MsgBuffer_GetTextBoxDisplayIcon(MsgBuffer* buf);
+void MsgBuffer_SetTextBoxDisplayIcon(MsgBuffer* buf, u8 icon);
+u16 MsgBuffer_GetNextMsg(MsgBuffer* buf);
+void MsgBuffer_SetNextMsg(MsgBuffer* buf, u16 type);
+u16 MsgBuffer_GetFirstItemRupees(MsgBuffer* buf);
+void MsgBuffer_SetFirstItemRupees(MsgBuffer* buf, u16 pos);
+u16 MsgBuffer_GetSecondItemRupees(MsgBuffer* buf);
+void MsgBuffer_SetSecondItemRupees(MsgBuffer* buf, u16 pos);
+MsgBufferSContent* MsgBuffer_GetContentPtr(MsgBuffer* buf);
 
 #endif
